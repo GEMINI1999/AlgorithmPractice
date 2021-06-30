beb42242ff8d63bfd1d4c8e2caceeb345954186a
yingyp <yyp@jackyun.com>
2021-06-30 16:05:58 +0800

    剑指37

---
+++ src/com/yyp/leetcode/SwordOffer37.java
@@ -0,0 +1,29 @@
+package com.yyp.leetcode;
+
+/**
+ * @author yingyaopeng
+ * @since 2021/6/30 14:38
+ * Copyright(C) 2021 Hangzhou Differsoft Co., Ltd. All rights reserved.
+ */
+public class SwordOffer37 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
+
